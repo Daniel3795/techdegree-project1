cac730785db4ccef080bb8fad80f852c7cfe04be
Daniel3795 <59145303+Daniel3795@users.noreply.github.com>
2020-03-28 12:31:38 -0700

    Made New Print Quote Function

--- js/script.js
+++ js/script.js
@@ -10,79 +10,77 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-const quotes = [
+var quotes = [
   {
-      quote: '"Don\'t be so humble, you\'re not that great"',
-      source: "Golda Meir",
-      citation: "As Good As Golda: The Warmth and Wisdom of Israel's Prime Minister",
-      year: "1970"
-  },
-  {
-      quote: '"If we change ourselves, the tendencies in the world would also change."',
-      source: "Mohandas Gandhi",
-      citation: "The Collected Works of Mahatma Gandhi, volume XII",
-      year: "1913"
-  },
-  {
-      quote: '"Do I not destroy my enemies when I make them my friend?"',
-      source: "Abraham Lincoln",
-      citation: "The Gettysburg Address",
-      year: "1863"
-  },
-  {
-      quote: '"Only thing we have to fear is fear itself."',
-      source: "Franklin D. Roosevelt",
-      citation: "Inaugural Address",
-      year: "1933"
-  },
-  {
-      quote: '"Life is what happens when you\'re busy making other plans."',
-      source: "John Lennon",
-      citation: "Beautiful Boy (Darling Boy)",
-      year: "1980"
-  },
-  {
-      quote: '"I\'d rather not live like there isn\'t a god, than die and find out there really is."',
-      source: "Kendrick Lamar",
-      citation: "Faith",
-      year: "2009"
-  }
-  ];
-
-
+    quote: "Don't be so humble, you\'re not that great",
+    source: "Golda Meir",
+    citation: "As Good As Golda: The Warmth and Wisdom of Israel's Prime Minister",
+    year: 1970
+},
+{
+    quote: "If we change ourselves, the tendencies in the world would also change.",
+    source: "Mohandas Gandhi",
+    citation: "The Collected Works of Mahatma Gandhi, volume XII",
+    year: 1913
+},
+{
+    quote: "Do I not destroy my enemies when I make them my friend?",
+    source: "Abraham Lincoln",
+    citation: "The Gettysburg Address",
+    year: 1863
+},
+{
+    quote: "Only thing we have to fear is fear itself.",
+    source: "Franklin D. Roosevelt",
+    citation: "Inaugural Address",
+    year: 1933
+},
+{
+    quote: "Life is what happens when you\'re busy making other plans.",
+    source: "John Lennon",
+    citation: "Beautiful Boy (Darling Boy)",
+    year: 1980
+},
+{
+    quote: "I'd rather not live like there isn't a god, than die and find out there really is.",
+    source: "Kendrick Lamar",
+    citation: "Faith",
+    year: 2009
+}
+];
+  
 /***
  * `getRandomQuote` function
 ***/
-function getRandomQuote() {
-  let random = Math.floor((Math.random() * quotes.length));
-  return quotes[random];
+  function getRandomQuote() {
+    let random = Math.floor((Math.random() * quotes.length));
+    return quotes[random];
 };
-
-
+   
 /***
  * `printQuote` function
 ***/
-function printQuote() {
-  let quoteList = getRandomQuote();
-  let message = ''
-  message += '<p class="quote">' + quoteList.quote + '</p>';
-  message += '<div><p1 class="source">' + quoteList.source +'<br></br></p1></div>';
-  if (quoteList.citation) {
-      message += '<div><span class="citation">' + quoteList.citation + '</span></div>';
-  }
-  if (quoteList.year) {
-      message += '<div><span class="year">' + quoteList.year + '</span></div>';
-  }
-  message + '</p>';
-  return document.write(message);
-};
+  const printQuote = () => {
+    var quote = getRandomQuote();
+    var htmlString = '<p class="quote">' + quote.quote + '</p><p class="source">' + quote.source;
+      if ('citation' in quote){
+        htmlString = htmlString + '<span class="citation">' + quote.citation + '</span>';
+      }
+      if ('year' in quote){
+        htmlString = htmlString + '<span class="year">' + quote.year + '</span>';
+      }
+      
+/* Updated 'quote-box' div with new string and added closing paragrapgh tag */
+    document.getElementById('quote-box').innerHTML = htmlString;
+    htmlString = htmlString + '</p>';
+    
+/*Added own "loadQuote" listener*/
+    document.getElementById('loadQuote');
+  };
 
-printQuote();
-
-
+  
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
